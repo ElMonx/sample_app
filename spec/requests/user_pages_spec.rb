3e1f8e9e395fe2a19186f17f5f95ea78b695277c
Ramon <ramon@softwareallies.com>
2012-05-14 14:47:43 -0500

    Final exercises of chapter 5

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,16 +1,15 @@
 require 'spec_helper'
 
 describe "UserPages" do
-  describe "User pages" do
+	describe "User pages" do
 
-  	subject { page }
+  		subject { page }
 
-  	describe "signup page" do
-  		before { visit signup_path }
+  		describe "signup page" do
+  			before { visit signup_path }
 
-  		it { should have_selector('h1',		text: 'Sign up') }
-  		it {should have_selector('title', text: full_title('Sign up')) }
-  		#jjj
-    end
-  end
+  			it { should have_selector('h1',		text: 'Sign up') }
+  			it { should have_selector('title', 	text: full_title('Sign up')) }
+    	end
+	end
 end
