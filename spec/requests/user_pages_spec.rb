fcc274ddcdd3f61c6f63dc283f30d4056a82c9b9
Ramon <ramon@softwareallies.com>
2012-05-29 11:37:33 -0500

    Chaper 8 exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -8,15 +8,15 @@ describe "User pages" do
 		let(:user) { FactoryGirl.create(:user) }
 		before { visit user_path(user) }
 
-		it { should have_selector("h1", text: user.name)}
-		it { should have_selector('title', text: user.name) }
+		it { should have_level1_header(user.name)}
+		it { should have_title(user.name) }
 	end
 
 	describe "signup page" do
 		before { visit signup_path }
 
-		it { should have_selector('h1',		text: 'Sign up') }
-		it { should have_selector('title', 	text: full_title('Sign up')) }
+		it { should have_level1_header('Sign up') }
+		it { should have_title(full_title('Sign up')) }
 	end
 
 	describe "signup" do
@@ -32,29 +32,22 @@ describe "User pages" do
 			describe "after submission" do
 				before { click_button submit }
 
-				it { should have_selector('title', text: 'Sign up') }
-				it { should have_content('error') }
+				it { should have_title('Sign up') }
+				it { should have_error_message('error') }
 			end
 		end
 
 		describe "with valid information" do
-			before do
-				fill_in "Name",			with: "Example User"
-				fill_in "Email",		with: "user@example.com"
-				fill_in "Password",		with: "foobar"
-				fill_in "Confirmation",	with: "foobar"
-			end
-
 			it "should create a user" do
-				expect { click_button submit }.to change(User, :count).by(1)
+				expect { valid_signup }.to change(User, :count).by(1)
 			end
 
 			describe "after saving the user" do
-				before { click_button submit }
+				before { valid_signup }
 				let(:user) { User.find_by_email('user@example.com') }
 
-				it { should have_selector('title', text: user.name) }
-				it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+				it { should have_title(user.name) }
+				it { should have_success_message('Welcome') }
 				it { should have_link('Sign out') }
 			end
 		end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,1 +1,43 @@
 include ApplicationHelper
+
+# Methods
+
+def valid_signin(user)
+	fill_in "Email",		with: user.email
+	fill_in "Password", 	with: user.password
+	click_button "Sign in"
+end
+
+def valid_signup
+	fill_in "Name",			with: "Example User"
+	fill_in "Email",		with: "user@example.com"
+	fill_in "Password",		with: "foobar"
+	fill_in "Confirmation",	with: "foobar"
+	click_button submit
+end
+
+# Rspec Custom Matchers
+
+RSpec::Matchers.define :have_error_message do |message|
+	match do |page|
+		page.should have_selector('div.alert.alert-error', text: message)
+	end
+end
+
+RSpec::Matchers.define :have_success_message do |message|
+	match do |page|
+		page.should have_selector('div.alert.alert-success', text: message)
+	end
+end
+
+RSpec::Matchers.define :have_title do |title|
+	match do |page|
+		page.should have_selector('title', text: title)
+	end
+end
+
+RSpec::Matchers.define :have_level1_header do |text|
+	match do |page|
+		page.should have_selector("h1", text: text)
+	end
+end
